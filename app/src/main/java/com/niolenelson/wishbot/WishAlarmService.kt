52a4e7f708b890635df37f2454678cf4c21bdee9
Niole Nelson <niolenelson@gmail.com>
2018-12-22 23:55:59 -0800

    Can show alert

--- app/src/main/java/com/niolenelson/wishbot/WishAlarmService.kt
+++ app/src/main/java/com/niolenelson/wishbot/WishAlarmService.kt
@@ -1,26 +1,30 @@
 package com.niolenelson.wishbot
 
-import android.app.AlarmManager
+import android.app.*
 import android.app.AlarmManager.RTC_WAKEUP
-import android.app.PendingIntent
 import android.app.PendingIntent.FLAG_CANCEL_CURRENT
-import android.app.Service
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.graphics.Color
 import android.os.Binder
 import android.util.Log
 import android.os.IBinder
 import android.provider.AlarmClock
 import android.support.v4.app.AlarmManagerCompat.setAlarmClock
+import android.support.v4.app.NotificationCompat
+import android.support.v4.app.NotificationManagerCompat
 import java.util.*
 
 /**
  * every time a wish time triggers, we set the next wish time
  */
 class WishAlarmService : Service() {
-    private var nextWishTime = Date().time + 5000
+
+    private val channelId = "wishalarmbot"
+
+    private var notificationManager: NotificationManager? = null
 
     private val mBinder = WishServiceBinder()
 
@@ -31,14 +35,19 @@ class WishAlarmService : Service() {
 
     val receiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
-            //Never gets hit
-            println("RECEIVE")
-            println(intent)
+            val notification = NotificationCompat.Builder(applicationContext, channelId)
+                .setPriority(0)
+                .setSmallIcon(R.drawable.ic_android_black_24dp)
+                .setContentText("MAKE A WISH")
+                .build()
+            notificationManager?.notify(0, notification)
         }
     }
 
     override fun onCreate() {
         super.onCreate()
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
         this.registerReceiver(receiver, IntentFilter("STUFF"))
         val intent = Intent()
         intent.action = "STUFF"
@@ -46,6 +55,11 @@ class WishAlarmService : Service() {
         val info = AlarmManager.AlarmClockInfo(Date().time + 5000, pendingIntent)
         val alarmManager: AlarmManager = this.getSystemService(Context.ALARM_SERVICE) as AlarmManager
         alarmManager.setAlarmClock(info, pendingIntent)
+
+        notificationManager = applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        val channel = NotificationChannel(channelId, channelId, NotificationManager.IMPORTANCE_HIGH)
+        notificationManager?.createNotificationChannel(channel)
     }
 
     override fun onBind(intent: Intent): IBinder? {
